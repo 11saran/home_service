42b48d541d84a19701257d61ec5d42500b81ab8d
11saran <1211saran@gmail.com>
2025-02-27 00:57:44 +0530

    added autOption.js file

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,30 +1,5 @@
-import NextAuth from "next-auth/next";
-
-
-
-export const authOptions = {
-  providers: [
-    {
-      id: "descope",
-      name: "Descope",
-      type: "oauth",
-      wellKnown: `https://api.descope.com/P2tU28dPuM7BZOciz5Et1DLE0D5C/.well-known/openid-configuration`,
-      authorization: { params: { scope: "openid email profile" } },
-      idToken: true,
-      clientId: "P2tU28dPuM7BZOciz5Et1DLE0D5C",
-      clientSecret: "<Descope Access Key>",
-      checks: ["pkce", "state"],
-      profile(profile) {
-        return {
-          id: profile.sub,
-          name: profile.name,
-          email: profile.email,
-          image: profile.picture,
-        };
-      },
-    },
-  ],
-};
+import NextAuth from "next-auth";
+import { authOptions } from "@/lib/authOptions"; // Ensure correct path
 
 const handler = NextAuth(authOptions);
 
